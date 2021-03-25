dc27a7705fd1c053c87ec554149f142d4c3864e9
izaanjahangir <izaanjahangir2@gmail.com>
2021-03-26 00:23:41 +0500

    added CupertinoDatePicker

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import "package:date_picker_test/components/button/button.dart";
@@ -5,6 +6,40 @@ import "package:date_picker_test/components/button/button.dart";
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void showIOSDatePicker() {
+      // showCupertinoModalPopup is a built-in function of the cupertino library
+      showCupertinoModalPopup(
+          context: context,
+          builder: (_) => Container(
+                height: 260,
+                color: Color.fromARGB(255, 255, 255, 255),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 200,
+                      child: CupertinoDatePicker(
+                          initialDateTime: DateTime.now(),
+                          onDateTimeChanged: (val) {}),
+                    ),
+
+                    // Close the modal
+                    CupertinoButton(
+                      child: Text('OK'),
+                      onPressed: () => Navigator.of(context).pop(),
+                    )
+                  ],
+                ),
+              ));
+    }
+
+    openAndroidDatePicker() async {
+      await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1970, 01, 01),
+          lastDate: DateTime(2099, 12, 31));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
@@ -15,8 +50,15 @@ class Home extends StatelessWidget {
         child: Column(
           children: [
             Button(
-              label: "Open Picker",
-              onPress: () {},
+              label: "Open Android Style Date Picker",
+              onPress: openAndroidDatePicker,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Button(
+              label: "Open IOS Style Date Picker",
+              onPress: showIOSDatePicker,
             )
           ],
         ),
